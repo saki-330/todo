c6a17a2c8afb6caa4f49a4f15dcdf0c1fc7c09e9
saki <saki.3.30.s@gmail.com>
2025-02-12 13:45:22 +0900

    開発途中

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -7,13 +7,25 @@
 @section('content')
 
 <div class="todo__alert">
+    @if (session('message'))
     <div class="todo__alert--success">
-        Todoを作成しました
+        {{ session('message') }}
     </div>
+    @endif
+    @if ($errors->any())
+    <div class="todo__alert--danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 </div>
 
 <div class="todo__content">
-    <form class="create-form">
+    <form class="create-form" action="/todos" method="post">
+        @csrf
         <div class="create-form__item">
             <input class="create-form__item-input" type="text" name="content">
         </div>
@@ -26,11 +38,15 @@
             <tr class="todo-table__row">
                 <th class="todo-table__header">Todo</th>
             </tr>
+            @foreach ($todos as $todo)
             <tr class="todo-table__row">
                 <td class="todo-table__item">
-                    <form class="update-form">
+                    <form class="update-form" action="/todos/update" method="post">
+                        @method('PATCH')
+                        @csrf
                         <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test">
+                            <input class="update-form__item-input" type="text" name="content" value="{{ $todo['content'] }}">
+                            <input type="hidden" name="id" value="{{ $todo['id'] }}">
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
@@ -38,13 +54,17 @@
                     </form>
                 </td>
                 <td class="todo-table__item">
-                    <form class="delete-form">
+                    <form class="delete-form" action="/todos/delete" method="POST">
+                        @method('DELETE')
+                        @csrf
                         <div class="delete-form__button">
+                            <input type="hidden" name="id" value="{{ $todo['id'] }}">
                             <button class="delete-form__button-submit" type="submit">削除</button>
                         </div>
                     </form>
                 </td>
             </tr>
+            @endforeach
         </table>
     </div>
 </div>
--- src/routes/web.php
+++ src/routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy']);
