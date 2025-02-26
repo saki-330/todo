d6adfceaaa4da621f4067188a81a7f320ff901c0
saki <saki.3.30.s@gmail.com>
2025-02-26 11:24:59 +0900

    開発途中

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,7 @@ class CategoryController extends Controller
         return view('category', compact('categories'));
     }
 
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
         $category = $request->only(['name']);
         Category::create($category);
@@ -22,4 +23,18 @@ class CategoryController extends Controller
         return redirect('/categories')->with('message', 'カテゴリを作成しました');
     }
 
+    public function update(CategoryRequest $request)
+    {
+        $category = $request->only(['name']);
+        Category::find($request->id)->update($category);
+
+        return redirect('/categories')->with('message', 'カテゴリを更新しました');
+    }
+
+    public function destroy(Request $request)
+    {
+        Category::find($request->id)->delete();
+        
+        return redirect('/categories')->with('message', 'カテゴリを削除しました');
+    }
 }
